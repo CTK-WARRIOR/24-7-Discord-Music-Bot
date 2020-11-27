0a6340ae272d402ef4fb4eaa6051c9c98d1d6984
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2020-11-27 23:13:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { TOKEN, CHANNEL, SERVER, STATUS, LIVE } = require("./config.json");
+const discord = require("discord.js");
+const client = new discord.Client();
+const ytdl = require('ytdl-core');
+
+client.on('ready', async () => {
+  client.user.setActivity(STATUS + " 😎")
+  let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
+
+  if(!channel) return;
+  const connection = await channel.join();
+  connection.play(ytdl(LIVE))
+})
+
+setInterval(async function() {
+  if(!client.voice.connections.get(SERVER)) {
+    let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
+    if(!channel) return;
+
+    const connection = await channel.join()
+    connection.play(ytdl(LIVE))
+  }
+}, 20000)
+
+client.login(TOKEN)
