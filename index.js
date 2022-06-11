6e43aa1fb532a556cdbbfa5468dfcdfd56549750
Aryaman Mishra <aryaman.mishra73@gmail.com>
2022-06-11 17:18:41 +0000

    Updated with change stream command and bot interactivity.

--- index.js
+++ index.js
@@ -22,6 +22,55 @@ if (!TOKEN) {
   console.log("Please provide a valid Youtube URL.");
   process.exit(1);
 }
+
+const fs = require('fs');
+const fileName = './config.json';
+const file = require(fileName);
+const prefix = "."; 
+const flag=1;
+
+// Turn bot off (destroy), then turn it back on
+function resetBot(channel) {
+    // send channel a message that you're resetting bot [optional]
+    channel.send('Resetting...')
+    .then(msg => client.destroy())
+    .then(() => client.login(TOKEN));
+}
+
+client.on('message', message => {
+  const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} 
+  else if (command === 'change') {
+		if (!args.length) {
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		} else {
+      file.LIVE = args[0];
+fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+  if (err) return console.log(err);
+  console.log(JSON.stringify(file));
+  console.log('writing to ' + fileName);
+});
+      //console.clear();
+      //client.destroy()
+      //client.login(TOKEN);
+      
+			return message.channel.send('Stream Changed');
+      resetBot(message.channel);
+      switch(flag)
+        {
+          case 1:break;
+        }
+		}
+
+		//message.channel.send(`First argument: ${args[0]}`);
+	}
+});
+
+
+
 require('./server')();
 // login
 client.login(TOKEN).then(() => {
